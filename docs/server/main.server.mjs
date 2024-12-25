9adda881c50af083cd46c53f8ef7a9535f930a28
Leandro Dailoff <leandrodailoff@outlook.com>
2024-12-25 17:49:05 -0300

    Prod

---
+++ docs/server/main.server.mjs
@@ -0,0 +1,2 @@
+import './polyfills.server.mjs';
+import{a}from"./chunk-OKEQ5NRF.mjs";import"./chunk-DOT4HYG5.mjs";import"./chunk-2RNLY2GH.mjs";import"./chunk-WV35DISR.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
